d6aa51bd51333b18340e538b4f2bf27f6dc86aff
Kornel Kwak <kornelkwak@Kornels-MacBook-Pro.local>
2023-10-20 10:27:54 +0200

    remove node_modules

--- node_modules/@strapi/admin/admin/custom.d.ts
+++
@@ -1,8 +0,0 @@
-export {};
-
-import { StrapiTheme } from '@strapi/design-system';
-
-declare module 'styled-components' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  export interface DefaultTheme extends StrapiTheme {}
-}

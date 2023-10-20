a2e41decd83ebe8ea9593580540c45f86e40b8d1
Kornel Kwak <kornelkwak@Kornels-MacBook-Pro.local>
2023-10-20 10:25:40 +0200

    project data

---
+++ node_modules/@strapi/admin/admin/src/contexts/index.js
@@ -0,0 +1,3 @@
+export { default as AdminContext } from './Admin';
+export { ConfigurationsContext } from './configuration';
+export { default as PermissionsDataManagerContext } from './PermisssionsDataManagerContext';
---
+++ node_modules/@strapi/admin/admin/src/hooks/index.js
@@ -0,0 +1,6 @@
+export { useConfigurations } from './useConfigurations';
+export { useContentTypes } from './useContentTypes';
+export { default as useMenu } from './useMenu';
+export { default as usePermissionsDataManager } from './usePermissionsDataManager';
+export { default as useSettingsForm } from './useSettingsForm';
+export { default as useSettingsMenu } from './useSettingsMenu';
